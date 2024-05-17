bc6d536b37c09075f96eb97f19b2f8b152b02d57
ian <somnii006@gmail.com>
2024-05-17 00:54:31 -0300

    Termine la funcionalidad

--- src/calculeHuman/Human.java
+++ src/calculeHuman/Human.java
@@ -32,10 +32,37 @@ public class Human implements HumanFunctions{
     public void setBirthday(String birthday){
         this.birthday = birthday;
     }
-    public void calcularEdad(int añoActual){
-        String[] partes = getBirthday().split("/");
+    public void calcularEdad(String añoActual){
+        int diaHoy = calculeDia(añoActual);
+        int mesHoy = calculeMes(añoActual);
+        int añoHoy = calculeAño(añoActual);
 
+        int dayBirth = calculeDia(getBirthday());
+        int monthBirth = calculeMes(getBirthday());
+        int yearBirth = calculeAño(getBirthday());
 
+        int añoDelAño = añoHoy-yearBirth;
+        if(mesHoy>monthBirth && diaHoy > dayBirth){
+            System.out.println("Usted tiene "+ (añoDelAño-1) + " Años");
+        }else{
+            System.out.println("Usted tiene "+añoDelAño + " Años");
+
+        }
+    }
+    public int calculeDia(String fecha){
+        String[] parte = getBirthday().split("/");
+        int dia =  Integer.parseInt(parte[0]);
+        return dia;
+    }
+    public int calculeMes(String fecha){
+        String[] parte = getBirthday().split("/");
+        int mes = Integer.parseInt(parte[1]);
+        return mes;
+    }
+    public int calculeAño(String fecha){
+        String[] parte = getBirthday().split("/");
+        int anio = Integer.parseInt(parte[2]);
+        return anio;
     }
     public String esPesoIdeal(){
         String respuesta = "";
